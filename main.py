178109a706c6a72cce99e46d5d9d26ad174b129d
TimofeevAndrei <i.pika4u@gmail.com>
2023-01-22 22:25:52 +0700

    begin

---
+++ main.py
@@ -0,0 +1,16 @@
+import sqlalchemy
+from models import create_tables
+
+from sqlalchemy.orm import  sessionmaker
+from settings import PASS
+
+DSN = PASS
+engine = sqlalchemy.create_engine(DSN)
+
+create_tables(engine)
+
+Session = sessionmaker(bind=engine)
+session = Session()
+
+
+session.close()
